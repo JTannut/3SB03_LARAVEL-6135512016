524f421d9fe9ca90484792f4a55f68bd9bb38835
JTannut <tannut_tawonsan@hotmail.com>
2021-02-10 10:00:18 +0700

    version 1

---
+++ resources/views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<PHP> echo '<link rel="index.css" type="text/css" href="indexcss.php"></head>'; <PHP>
+    <body class="bodybg">
+<h2>FROM 6135512016</h2>
+<form name="form1" method="get" action="/sendbasicemail">  
+  Name: <input type="text" name="name"  placeholder="name" >
+  <br><br>
+  E-mail: <input type="email" name="email"  placeholder="email" >
+  <br><br>
+  Message <input type="text" name="message"  placeholder="message" >
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+  <div>
+
+  </div>
+</form>
+    </body>
+</html>
---
+++ resources/views/indexcss.php
@@ -0,0 +1,267 @@
+<?php
+header('Countent-type: text/css; charset:UTF-8');
+?>
+
+* {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.bodybg {
+    background-image: url("wp.jpg");
+    background-size: 100%;
+    display: block;
+}
+
+
+/* h1 {
+    /*padding-left: 200px;*/
+
+
+/* text-align: center;
+    margin: 5px 10px 20px 30px;
+    height: 320px;
+    width: 300px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); */
+
+
+/* } */
+
+.img {
+    width: 180px;
+}
+
+.input {
+    transform: translate(0%, 100%);
+    border-radius: 10px;
+    font-size: 20px;
+    margin: 4px 2px;
+    padding: 5px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+h2 {
+    /*padding-left: 200px;*/
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+h3 {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+    text-size-adjust: 50px;
+    width: 400px;
+    margin: auto;
+    background-color: rgb(255, 255, 255);
+}
+
+.login2 {
+    display: flex;
+    justify-content: center;
+    text-size-adjust: 20px;
+    border-radius: 16px;
+    margin: auto;
+    background-color: rgb(255, 255, 255);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+    max-width: 250px;
+    padding: 30px 10px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    /* -webkit-transform: translate(-50%,-50%); */
+    -o-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    width: 100%;
+    display: block;
+}
+
+.stk {
+    background-image: url("wp.jpg");
+    border: 3px solid whitesmoke;
+    border: none;
+    color: white;
+    padding: 9px 87px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 1px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius: 16px;
+    background-size: 200%;
+}
+
+.login {
+    display: flex;
+    justify-content: center;
+    text-size-adjust: 20px;
+    border-radius: 16px;
+    margin: auto;
+    background-color: rgb(255, 255, 255);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+    max-width: 250px;
+    padding: 70px 10px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    /* -webkit-transform: translate(-50%,-50%); */
+    -o-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    width: 100%;
+    display: block;
+}
+
+.lbut {
+    transform: translate(0%, 160%);
+    background-image: url("wp.jpg");
+    border: 3px solid whitesmoke;
+    border: none;
+    color: white;
+    padding: 10px 90px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 1px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius: 16px;
+    background-size: 200%;
+}
+
+.gbut {
+    transform: translate(0%, 150%);
+    background-image: url("wp.jpg");
+    border: 3px solid whitesmoke;
+    border: none;
+    color: white;
+    padding: 10px 96px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 0px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius: 16px;
+    background-size: 200%;
+}
+
+.buttonn {
+    background-image: url("wp.jpg");
+    border: 3px solid whitesmoke;
+    border: none;
+    color: white;
+    padding: 10px 32px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius: 16px;
+    background-size: 200%;
+}
+
+.but {
+    background-image: url("wp.jpg");
+    border: 3px solid whitesmoke;
+    border: none;
+    color: white;
+    padding: 8px 60px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius: 16px;
+    background-size: 300%;
+}
+
+.button {
+    border: none;
+    color: white;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius: 16px;
+}
+
+.button0 {
+    background-color: white;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    color: black;
+}
+
+.button1 {
+    background-color: white;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    color: rgb(255, 255, 255);
+}
+
+.button1:hover {
+    background-color: #dad73e;
+    color: rgb(0, 0, 0);
+}
+
+.button2 {
+    background-color: white;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    color: black;
+}
+
+.button3 {
+    background-color: white;
+    color: black;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.button2:hover {
+    background-color: #008CBA;
+    color: white;
+}
+
+.button3:hover {
+    background-color: #632875;
+    color: white;
+}
+
+.Block {
+    align-items: center;
+    background-color: tomato;
+    color: white;
+    border: 2px solid black;
+    margin: 20px;
+    padding: 20px;
+}
+
+.button2 {
+    background-color: white;
+    color: black;
+}
